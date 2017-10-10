2b070ac3fd323c264158bb808144732ac4f1283f
Jonathan Brandwein <jonathan.brandwein@gmail.com>
2017-10-10 19:56:51 -0400

    created model

--- models/Order.js
+++ models/Order.js
@@ -2,40 +2,21 @@ const db = require('./conn');
 const Sequelize = db.Sequelize;
 
 const Order = db.define('order', {
-  address: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      isEmail: true
-    }
+  isCart: {
+    type: Sequelize.BOOLEAN
   },
   shippingPrice: {
     type: Sequelize.FLOAT
   },
-  status: {
-    type: Sequelize.STRING
-  },
-  date: {
-    type: Sequelize.DATE,
-    allowNull: false
+  tax: {
+    type: Sequelize.FLOAT
   },
   total: {
     type: Sequelize.FLOAT
   },
-  tax: {
-    type: Sequelize.FLOAT
-  }, 
   weight: {
     type: Sequelize.FLOAT
   }
 });
 
-
-
 module.exports = Order;
-
-
