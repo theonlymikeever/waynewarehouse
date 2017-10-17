751fe667944c229aa8d016b6a198e04694655195
Mike Kerslake <theonlymikeever@gmail.com>
2017-10-17 13:46:06 -0400

    Update LineItem model to have price

--- models/LineItem.js
+++ models/LineItem.js
@@ -1,16 +1,31 @@
 const db = require('./conn');
 const Sequelize = db.Sequelize;
+const Product = require('./Product');
 
 const LineItem = db.define('lineItem', {
     quantity: {
       type: Sequelize.INTEGER,
       defaultValue: 1
+    },
+    price: {
+      type: Sequelize.FLOAT
     }
 });
 
-// LineItem.beforeCreate = function (lineItem){
-//   lineItem.quantity++
-//   return lineItem.save()
-// }
+LineItem.hook('afterCreate', (lineItem) => {
+  lineItem.setPrice(lineItem)
+})
+
+LineItem.hook('afterSave', (lineItem) => {
+   lineItem.setPrice(lineItem)
+})
+
+LineItem.prototype.setPrice = function (lineItem) {
+    return Product.findOne({where: { id: lineItem.productId }})
+    .then(product => {
+      lineItem.price = product.price * lineItem.quantity
+      return lineItem.save()
+    })
+}
 
 module.exports = LineItem;
