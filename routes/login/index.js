a48bad582b9286d17256b61bf85ad63cc9cd6397
Robert van der Mark <robert.vandermark@gmail.com>
2017-10-18 16:47:27 -0400

    remove auto Testie login

--- routes/login/index.js
+++ routes/login/index.js
@@ -3,7 +3,7 @@ const User = require('../../models/User');
 const session = require('express-session');
 
 router.get('/', (req, res, next) => {
-	User.findById(req.session.userId || 3)
+	User.findById(req.session.userId)
 		.then(user => {
 			res.send(user);
 		})
@@ -28,7 +28,7 @@ router.post('/', (req, res, next) => {
 router.delete('/', (req, res, next) => {
 	console.log('routes/logout');
 	req.session.userId = '';
-	// console.log('session: ', req.session);
+	console.log('session: ', req.session);
 	res.sendStatus(200);
 
 });
