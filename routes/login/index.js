720ede304de2ca8bbfcd9468ac09f9d36d0c5676
Jonathan Brandwein <jonathan.brandwein@gmail.com>
2017-10-22 14:46:56 -0400

    took out testie

--- routes/login/index.js
+++ routes/login/index.js
@@ -4,7 +4,7 @@ const Address = require('../../models/User');
 const session = require('express-session');
 
 router.get('/', (req, res, next) => {
-	User.findById(req.session.userId || 3, {
+	User.findById(req.session.userId, {
 		include: [{ all: true }]
 	})
 		.then(user => {
