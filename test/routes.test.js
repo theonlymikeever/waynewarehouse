8b82297c8cad88240e5577be9737633f518407f5
Mike Kerslake <theonlymikeever@gmail.com>
2017-10-23 22:15:40 -0400

    testing for products 80% done

--- test/routes.test.js
+++ test/routes.test.js
@@ -22,17 +22,72 @@ const Review = require('../models/Review');
 
 describe('GET /products', function() {
 
-  before(function() {
-    return db.sync({force: true})
-  })
-
-
   //Main product get list
   it('returns all products', function() {
     return agent
     .get('/api/products')
     .expect(200)
-    // .expect(res.body).to.be.an.instanceOf(Array);
+    .expect(function(res){
+      expect(res.body).to.be.an.instanceOf(Array);
+      // expect(res.body)
+    })
   })
 });
+
+describe('GET, POST, PUT, DEL /products/:productId', function(){
+  //Starting with the list of our first seeded products
+  //occastionally on startup they seed out of order
+  //so we'll use an indexOf to ensure it's at least building
+  //one of these
+  const intialProducts = ['Utility Belt', 'Invisibility Cloak', 'Captain Jack\'s Compass', 'Bag End', 'Bane\'s Mask']
+
+
+  //Returns spefic products
+  it('returns a single product', function(){
+    return agent
+    .get('/api/products/1')
+    .expect(200)
+    .expect(function(res){
+      expect(intialProducts.indexOf(res.body.name)).to.not.equal(-1)
+    })
+  })
+  //Create a new product
+  it('creates a new product with proper info', function(){
+    const prod = {
+      name: 'The Elder Wand',
+      price: 300000,
+      description: 'The most powerful wand of them all',
+    }
+
+    return agent
+    .post('/api/products')
+    .send(prod)
+    .expect(201)
+    .then(function (){
+      return Product.findOne({ where: {name: 'The Elder Wand'}});
+    })
+    .then(function(prod) {
+      expect(prod).to.exist // eslint-disable-line no-unused-expressions
+    })
+  })
+  //Updating product information
+  it('can update a product\'s info, even multiple fields', function(){
+    const update = {
+      name: 'Pokeball',
+      price: 999
+    }
+
+   return agent
+    .put('/api/products/2')
+    .send(update)
+    .expect(200)
+    .then(function(){
+      return Product.findOne({where:{name:'Pokeball'}})
+    })
+    .then(function(prod){
+      expect(prod).to.exist // eslint-disable-line no-unused-expressions
+      expect(prod.price).to.equal('999')
+    })
+  })
+})
 
